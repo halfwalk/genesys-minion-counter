99120c854297be354876007a5811382f44310142
halfwalk <halfwalkmedia@gmail.com>
2021-08-05 22:35:57 -0400

    Update minionCountAdjuster.js

--- scripts/minionCountAdjuster.js
+++ scripts/minionCountAdjuster.js
@@ -1,17 +1,16 @@
 let imgs = [];
-const skull = "icons/svg/skull.svg";
 
-for (i=0; i<10;i++) {
-	let str = `modules/genesys-minion-counter/images/${i}.webp`;
+for (i=1; i<=10;i++) {
+	const str = `modules/genesys-minion-counter/images/${i}.webp`;
 	imgs.push(str);
 }
-let tenPlus = `modules/genesys-minion-counter/images/10.webp`;
+imgs.unshift("icons/svg/skull.svg");
 
 Hooks.on("createToken", (token) => {
 	if (token.actor.data.type == "minion") {
-		let count = token.actor.data.data.quantity.value;
+		const count = token.actor.data.data.quantity.value;
 		if (count < 10 && count > 0) token.data.effects.push(imgs[count]);
-		if (count >= 10) token.data.effects.push(tenPlus);
+		if (count >= 10) token.data.effects.push(imgs[10]);
 		if (count <= 0) token.data.effects.push(skull);
 	}
 });
@@ -29,38 +28,33 @@ async function updateIcon (token) {
 
 	// get all current custom status effects on token
 
-	for (let eff of token.data.effects) {
+	for (const eff of token.data.effects) {
 		currentEffects.push(eff);
 	}
 	
-	// if it has a counter icon already, get rid of it
+	// get rid of any existing counter icons
 	
-		for (let i of imgs) {
+		for (const i of imgs) {
 			if (token.data.effects.includes(i)) 
 				currentEffects.splice(currentEffects.indexOf(i),1);					
 		}
-		if (token.data.effects.includes(tenPlus)) 
-			currentEffects.splice(token.data.effects.indexOf(tenPlus));
-		if (token.data.effects.includes(skull))
-			currentEffects.splice(token.data.effects.indexOf(skull));
 	
 	// put the proper status icon on it
 	
 	if (minioncount < 10 && minioncount > 0) currentEffects.unshift(imgs[minioncount]);	
-	else if (minioncount >= 10) currentEffects.unshift(tenPlus);
-	else if (minioncount <= 0) currentEffects.unshift(skull);		
+	else if (minioncount >= 10) currentEffects.unshift(imgs[10]);
+	else currentEffects.unshift(imgs[0]);		
 	
 	// the setTimeout seems to get rid of the weird random console error ... i don't know why, but it works.
 	
 	setTimeout(async () => {
-	await token.update({effects:currentEffects});},15);
-	
+	await token.update({effects:currentEffects});},15);	
 }
 
 Hooks.on("updateActor", async (...args)=> {
 	const tokenName = args[0].data.token.name;
 	if (args[0].data.type == "minion" && !!canvas.tokens.placeables.find(i=> i.data.name == tokenName)) {
-		let token = canvas.tokens.placeables.find(i=> i.data.name == tokenName);
+		const token = canvas.tokens.placeables.find(i=> i.data.name == tokenName);
 		for (const i of canvas.tokens.placeables.filter(i=>i.data.name == tokenName)) {
 			await updateIcon(i.document);
 		}
